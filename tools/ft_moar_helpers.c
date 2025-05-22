0de02c0d7b14d83beee186a5fa6926a5f868add6
luvcie <lucpardo@student.42.fr>
2025-05-22 22:43:34 +0200

    add more helpers

---
+++ tools/ft_moar_helpers.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_moar_helpers.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/22 22:26:14 by lucpardo          #+#    #+#             */
+/*   Updated: 2025/05/22 22:28:09 by lucpardo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../ft_printf.h"
+
+int ft_unsigned(unsigned int u)
+{
+
+}
+
+int ft_hexadecimal(unsigned int nbr, int is_upper)
+{
+
+}
+
+int ft_pointer(void *ptr)
+{
+
+}
