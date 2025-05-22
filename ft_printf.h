6beacb51bfa5500801048c252b428015916f671d
luvcie <lucpardo@student.42.fr>
2025-05-22 19:53:08 +0200

    add ft_printf.h work in progress

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/22 19:50:00 by lucpardo          #+#    #+#             */
+/*   Updated: 2025/05/22 19:52:45 by lucpardo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+int	ft_printf(const char *str, ...);
+
+#endif
