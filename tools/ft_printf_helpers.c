fb6c9db1f25bd397e75af005c06743c273a286db
luvcie <lucpardo@student.42.fr>
2025-05-22 22:25:25 +0200

    add str helper

--- tools/ft_printf_helpers.c
+++ tools/ft_printf_helpers.c
@@ -6,11 +6,37 @@
 /*   By: lucpardo <lucpardo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 22:09:56 by lucpardo          #+#    #+#             */
-/*   Updated: 2025/05/22 22:10:00 by lucpardo         ###   ########.fr       */
+/*   Updated: 2025/05/22 22:25:11 by lucpardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../ft_printf.h"
 
+ft_string(char *str)
+{
+	if (str == NULL)
+	{
+		ft_putstr_fd("(null)", 1);
+		return (6);
+	}
+	ft_putstr_fd(str, 1);
+	return (ft_strlen(str)); 
+}
+
+ft_integer(int n)
+{
+}
+
+ft_unsigned(unsigned int u)
+{
+}
+
+ft_hexadecimal(unsigned int nbr, int is_upper)
+{
+}
+
+ft_pointer(void *ptr)
+{
+}
 // string
 // decimal and integer
 // unsigned
